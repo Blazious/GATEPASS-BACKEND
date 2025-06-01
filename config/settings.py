bd80f8c45b6165176f5b4be7df96f1940288a403
Blazious <blaziousmwambuwa@gmail.com>
2025-06-01 12:39:09 +0300

    hwbt

--- config/settings.py
+++ config/settings.py
@@ -129,9 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'gatepass' / 'static',
+    BASE_DIR / 'gatepass' / 'static' / 'branding',
 ]
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 
