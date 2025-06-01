9c1ea509bf215a038d26845d7368f269045603f6
Blazious <blaziousmwambuwa@gmail.com>
2025-06-01 14:08:22 +0300

    hwbt

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
