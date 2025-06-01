697787bdf088cd112edd3be7419503f6d173f2a8
Blazious <blaziousmwambuwa@gmail.com>
2025-06-01 11:59:59 +0300

    default database

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import dj_database_url
 
 from pathlib import Path
 from datetime import timedelta
@@ -83,13 +84,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # This points to the SQLite file in your project folder
     }
 }
+
+
 
 
 # Password validation
